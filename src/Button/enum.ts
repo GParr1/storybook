1b1555e22c53c28d901ef2151133ceab27b913a4
Parrone <giovanni.a.parrone@accenture.com>
2026-08-13 15:57:43 +0200

    remove unused file

--- src/Button/enum.ts
+++
@@ -1,7 +0,0 @@
-export enum ButtonType {
-  PRIMARY = 'primary',
-  SECONDARY = 'secondary',
-  TAG = 'tag',
-  NONE = 'none',
-  SUBMIT = 'submit'
-}
--- src/Text/types.ts
+++
@@ -1,5 +0,0 @@
-import type {TextProps} from "react-native";
-
-export type NativeTextProps = TextProps & {
-    as?: 'h1' | 'h5' | 'span' | 'p'
-}
